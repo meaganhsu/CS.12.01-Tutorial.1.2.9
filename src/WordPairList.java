0e8a4293c9bbaf7c578528ebff7d8e4471e609e7
meaganhsu <myth72@student.cis.edu.hk>
2024-08-29 10:12:05 +0800

    done

---
+++ src/WordPairList.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class WordPairList {
+    private ArrayList<WordPair> allPairs;
+    public WordPairList(String[] words) {
+        allPairs = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i+1; j < words.length; j++) {
+                allPairs.add(new WordPair(words[i], words[j]));
+            }
+        }
+    }
+    public int numMatches() {
+        int cnt = 0;
+        ArrayList<WordPair> copy = allPairs;
+
+        for (int i = 0; i < copy.size(); i++) {
+            int x = 0;
+
+            for (int j = 0; j < copy.size(); j++) {        // checks for duplicate pairs
+                if (copy.get(i).getFirst().equals(copy.get(j).getFirst())) {
+                    if (copy.get(i).getSecond().equals(copy.get(j).getSecond())) x++;
+                }
+            }
+
+            if (x > 1) {
+                cnt++;
+
+                for (int k = 0; k < copy.size(); k++) {     // removes all duplicate pairs from temp arraylist
+                    if (copy.get(i).getFirst().equals(copy.get(k).getFirst())) {
+                        if (copy.get(i).getSecond().equals(copy.get(k).getSecond())) copy.remove(copy.get(k));
+                    }
+                }
+            }
+        }
+
+        return cnt;
+    }
+    public ArrayList<WordPair> getAllPairs() {
+        return allPairs;
+    }
+}
